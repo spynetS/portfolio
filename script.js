d6b5e1d12988139b0e73772a5ee336da48c05100
Alfred Roos <alfred@stensatter.se>
2022-10-18 18:26:36 +0200

    update

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function writeToClipboard() {
     // Alert the copied text
     alert("Copied the text: " + "alfred@stensatter.se");
   } 
+
+function on(){
+
+  let menu = document.getElementById("menu");
+  menu.style = "top:0%;  transition-duration: 500ms;"
+  console.log(menu)
+}
+
+function off(){
+
+  let menu = document.getElementById("menu");
+  menu.style = "top:-100%;  transition-duration: 500ms;"
+  console.log(menu)
+}
